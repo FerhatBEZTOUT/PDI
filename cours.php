c3833987f821c1b8a97c3aeafcdd26a84c08ddb3
Ferhat <ferhatbeztout@gmail.com>
2023-03-15 10:25:56 +0100

    reset cours

--- cours.php
+++ cours.php
@@ -3,11 +3,6 @@
 ?>
 
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, nostrum suscipit. Excepturi nihil veniam ea explicabo, velit asperiores! Reprehenderit, numquam?</p>
-
-<h1>sfydfydg</h1>
-
-<p>dsd</p>
 
 <?php 
 include_once __DIR__.'/view/footer.php';
