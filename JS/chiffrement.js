26386dc17cf72b9c6aadff2b3e9e29ee4bf0d7e4
SHAO <qichen_shao2000@qq.com>
2023-03-15 23:38:28 +0100

    shao

--- JS/chiffrement.js
+++ JS/chiffrement.js
@@ -287,6 +287,7 @@ function CreerGrille (clef)
 			grille += ch
 		}
 	}
+    $("#m1").val(grille[0]);
 	return grille
 }
 
@@ -362,7 +363,9 @@ function Playfair(clair, clef, chiffre, m11, m12, m13, m14, m15, m21, m22, m23, 
         clef = $("#txt-playfaircles");
         clair= $("#txt-chiffrement");
         chiffre=$("#txt-dechiffrement");
-        Playfair(clair, clef, chiffre, $("#m11"), m12, m13, m14, m15, m21, m22, m23, m24, m25, m31, m32, m33, m34, m35, m41, m42, m43, m44, m45, m51, m52, m53, m54, m55)
+        // Playfair(clair, clef, chiffre, $("#m11"), m12, m13, m14, m15, m21, m22, m23, m24, m25, m31, m32, m33, m34, m35, m41, m42, m43, m44, m45, m51, m52, m53, m54, m55)
+        standard(clair)
+        matrice = CreerGrille(clef);
     };
 
     function chiffrement_vigenere() {
