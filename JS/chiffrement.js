96f6870d00a00bd17c5de1339296382888a9990b
SHAO <qichen_shao2000@qq.com>
2023-03-15 22:08:23 +0100

    s

--- JS/chiffrement.js
+++ JS/chiffrement.js
@@ -204,8 +204,8 @@ $(document).ready(function () {
         // block_cle += '</tr >';
 
         block_cle ='<tr>';
-        block_cle +='<td bgcolor="#08FF7C"><textarea name="clair" rows="6" cols="30" wrap="virtual">CHIFFRE DE PLAYFAIR</textarea></td>';
-        block_cle +='<td  bgcolor="#FFCCCC" align="CENTER">';
+        block_cle +='<td bgcolor="#08FF7C" align="CENTER"><textarea name="clair" rows="6" cols="30" wrap="virtual">CHIFFRE DE PLAYFAIR</textarea></td>';
+        block_cle +='<td  bgcolor="#FFCCCC" align="CENTER" >';
         block_cle +='<input type="TEXT" name="m11" size="1"><input type="TEXT" name="m12" size="1"><input type="TEXT" name="m13" size="1"><input type="TEXT" name="m14" size="1"><input type="TEXT" name="m15" size="1"><br>';
         block_cle +='<input type="TEXT" name="m21" size="1"><input type="TEXT" name="m22" size="1"><input type="TEXT" name="m23" size="1"><input type="TEXT" name="m24" size="1"><input type="TEXT" name="m25" size="1"><br>';
         block_cle +='<input type="TEXT" name="m31" size="1"><input type="TEXT" name="m32" size="1"><input type="TEXT" name="m33" size="1"><input type="TEXT" name="m34" size="1"><input type="TEXT" name="m35" size="1"><br>';
