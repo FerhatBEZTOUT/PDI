518a5a0dcf361a6c09c754e7f7133ed9962f04cc
SHAO <qichen_shao2000@qq.com>
2023-03-17 16:25:57 +0100

    shao

--- JS/chiffrement.js
+++ JS/chiffrement.js
@@ -361,10 +361,10 @@ function Playfair(clair, clef, chiffre, m11, m12, m13, m14, m15, m21, m22, m23, 
 
     function chiffrement_playfair() {
         // Playfair($("#txt-chiffrement"),$("#txt-playfaircles"),$("#txt-dechiffrement"), $("#m11"), $("m12"), $("m13"), $("m14"), $("m15"), $("m21"), $("m22"), $("m23"), $("m24"), $("m25"), $("m31"),$("m32"), $("m33"), $("m34"), $("m35"), $("m41"), $("m42"), $("m43"), $("m44"), $("m45"), $("m51"), $("m52"), $("m53"), $("m54"), $("m55"))
-    clef=$("#txt-playfaircles")
-    clair=$("#txt-chiffrement")
+    clef=$("#txt-playfaircles");
+    clair=$("#txt-chiffrement");
     standard(clef);
-	var matrice  = '';
+	var grille  = '';
 	for(var nbr = 0; nbr < clef.value.length; nbr++){
 		ch= clef.value.charAt(nbr);
 		if (grille.indexOf (ch) < 0) {
@@ -377,7 +377,7 @@ function Playfair(clair, clef, chiffre, m11, m12, m13, m14, m15, m21, m22, m23, 
 			grille += ch;
 		}
 	}
-    matrice = CreerGrille(clef);
+    // matrice = CreerGrille(clef);
 	n=0;
 	standard(clair);
 	chiffre.value = ""; 
