671afc2e9cd4ef28f2d54465c6c929848318c4e1
SHAO <qichen_shao2000@qq.com>
2023-03-17 16:44:00 +0100

    shao

--- JS/chiffrement.js
+++ JS/chiffrement.js
@@ -295,7 +295,7 @@ $(document).ready(function () {
 
 
     function chiffrement_playfair() {
-        $('#txt-dechiffrement').val('asd');
+        $('#txt-dechiffrement').val(texte_en_clair);
     // clef = $("#txt-playfaircles").value;
     // clair = texte_en_clair;
 	// var matrice  = '';
