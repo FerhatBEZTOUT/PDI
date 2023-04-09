df3da997877c19225534bd5fbbe05ad06a744e72
Ferhat <ferhatbeztout@gmail.com>
2023-04-09 22:47:10 +0200

    exo2 php

--- exercices2.php
+++ exercices2.php
@@ -38,6 +38,17 @@ include_once __DIR__ . '/view/header.php';
     </div>
   </div>
 
+  <div class="row justify-content-center mb-2">
+    <div class="col-lg-8">
+      <div class="card">
+        <div class="card-body bg-dark rounded">
+            <h2 class="text-center text-light">Exercice 3</h2>
+          <div id="exo2-container"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 </div>
 
