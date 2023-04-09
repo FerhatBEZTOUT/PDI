706394d9b25034184395cdf92c3e0b66833a20e2
Ferhat <ferhatbeztout@gmail.com>
2023-04-09 23:54:14 +0200

    test3 et changement nom

--- exercices2.php
+++ exercices2.php
@@ -5,23 +5,24 @@ include_once __DIR__ . '/view/header.php';
 
 <nav class="chapter-nav">
   <ul>
-    <li><a href="#qcm-container">QCM</a></li>
-    <li><a href="#exo1-container">Chiffrement de César</a></li>
-    <li><a href="#exo2-container">Chiffrement de Vigénere</a></li>
-    <li><a href="#exo3-container">Chiffrement d'Affine</a></li>
-    <li><a href="#exo4-container">Chiffrement de Playfair</a></li>
-    <li><a href="#exo5-container">Test 1</a></li>
-    <li><a href="#exo6-container">Test 2</a></li>
+    <li><a href="#titre-qcm-container">QCM</a></li>
+    <li><a href="#titre-cesar-container">Chiffrement de César</a></li>
+    <li><a href="#titre-vigenere-container">Chiffrement de Vigénere</a></li>
+    <li><a href="#titre-affine-container">Chiffrement d'Affine</a></li>
+    <li><a href="#titre-playfair-container">Chiffrement de Playfair</a></li>
+    <li><a href="#titre-test1-container">Test 1</a></li>
+    <li><a href="#titre-test2-container">Test 2</a></li>
+    <li><a href="#titre-test3-container">Test 3</a></li>
   </ul>
 </nav>
 
 <div class="container my-5">
-
+  
   <div class="row justify-content-center mb-2">
     <div class="col-lg-8">
       <div class="card">
         <div class="card-body bg-dark rounded">
-            <h2 class="text-center text-light">QCM</h2>
+            <h2 id="titre-qcm-container" class="text-center text-light">QCM</h2>
           <div id="qcm-container"></div>
         </div>
       </div>
@@ -32,8 +33,8 @@ include_once __DIR__ . '/view/header.php';
     <div class="col-lg-8">
       <div class="card">
         <div class="card-body bg-dark rounded">
-            <h2 class="text-center text-light">Chiffrement de César</h2>
-          <div id="exo1-container"></div>
+            <h2 id="titre-cesar-container" class="text-center text-light">Chiffrement de César</h2>
+          <div id="cesar-container"></div>
         </div>
       </div>
     </div>
@@ -43,8 +44,8 @@ include_once __DIR__ . '/view/header.php';
     <div class="col-lg-8">
       <div class="card">
         <div class="card-body bg-dark rounded">
-            <h2 class="text-center text-light">Chiffrement de Vigenere</h2>
-          <div id="exo2-container"></div>
+            <h2 id="titre-vigenere-container" class="text-center text-light">Chiffrement de Vigenere</h2>
+          <div id="vigenere-container"></div>
         </div>
       </div>
     </div>
@@ -54,8 +55,8 @@ include_once __DIR__ . '/view/header.php';
     <div class="col-lg-8">
       <div class="card">
         <div class="card-body bg-dark rounded">
-            <h2 class="text-center text-light">Chiffrement d'Affine</h2>
-          <div id="exo3-container"></div>
+            <h2 id="titre-affine-container" class="text-center text-light">Chiffrement d'Affine</h2>
+          <div id="affine-container"></div>
         </div>
       </div>
     </div>
@@ -65,8 +66,8 @@ include_once __DIR__ . '/view/header.php';
     <div class="col-lg-8">
       <div class="card">
         <div class="card-body bg-dark rounded">
-            <h2 class="text-center text-light">Chiffrement de Playfair</h2>
-          <div id="exo4-container"></div>
+            <h2 id="titre-playfair-container" class="text-center text-light">Chiffrement de Playfair</h2>
+          <div id="playfair-container"></div>
         </div>
       </div>
     </div>
@@ -77,8 +78,8 @@ include_once __DIR__ . '/view/header.php';
     <div class="col-lg-8">
       <div class="card">
         <div class="card-body bg-dark rounded">
-            <h2 class="text-center text-light">Test 1</h2>
-          <div id="exo5-container"></div>
+            <h2 id="titre-test1-container" class="text-center text-light">Test 1</h2>
+          <div id="test1-container"></div>
         </div>
       </div>
     </div>
@@ -88,8 +89,19 @@ include_once __DIR__ . '/view/header.php';
     <div class="col-lg-8">
       <div class="card">
         <div class="card-body bg-dark rounded">
-            <h2 class="text-center text-light">Test 2</h2>
-          <div id="exo6-container"></div>
+            <h2 id="titre-test2-container" class="text-center text-light">Test 2</h2>
+          <div id="test2-container"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row justify-content-center mb-2">
+    <div class="col-lg-8">
+      <div class="card">
+        <div class="card-body bg-dark rounded">
+            <h2 id="titre-test3-container" class="text-center text-light">Test 3</h2>
+          <div id="test3-container"></div>
         </div>
       </div>
     </div>
